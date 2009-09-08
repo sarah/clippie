bba0a344d100e25e0fd632eb6116c04bc0608009
sarah <sarahg.gray@gmail.com>
2009-09-07 23:49:46 -0500

    clippie now can be told it is okay

--- lib/clippie/cucumber.rb
+++ lib/clippie/cucumber.rb
@@ -22,6 +22,15 @@ module Clippie
     def self.step_definitions(path)
       self.step_definitions_root = path
     end
+
+    def self.its_okay_clippie
+      @its_okay_clippie = true
+    end
+
+    def self.is_overly_helpful?
+      ! @its_okay_clippie
+    end
+    
     class UndefinedSteps
       def self.add(step)
         (@undefined_steps ||= []) << step
@@ -29,31 +38,41 @@ module Clippie
       def self.steps
         @undefined_steps
       end
-    end
+    end  
   end
+  
   def self.says(something)
     puts "Clippie Says: #{something}\n"
   end
 end
 
 if respond_to?(:After)
-  After do |scenario|
-    Cucumber::Cli::Main.step_mother.steps(:undefined).each {|step| Clippie::Cucumber::UndefinedSteps.add(step)}
+  def create_steps(steps, show_undefined_steps = false)
+    step_definitions = steps.map {|step|
+      multiline_arg_class = step.multiline_arg.nil? ? nil : step.multiline_arg.class
+        Cucumber::Cli::Main.step_mother.snippet_text(step.keyword, step.name, multiline_arg_class)
+      }
+    Clippie.says "I noticed you have some undefined steps"
+    if show_undefined_steps
+      puts step_definitions.join("\n\n")
+    end
+    Clippie.says "Would you like me to create them for you [Yn]?"
+    input = gets
+    if(input =~ /^n/)
+      Clippie.says "Awwww...Okay. I'll stop now.  I'll just leave you alone."
+    else
+      Clippie.says "Yay! I'm creating them for you now."
+      Clippie::Cucumber.define_steps(step_definitions.join("\n\n"))
+    end
   end
-end
+  
+  After do
+    Cucumber::Cli::Main.step_mother.steps(:undefined).each {|step| Clippie::Cucumber::UndefinedSteps.add(step)}
+    
+    create_steps(Clippie::Cucumber::UndefinedSteps.steps, true) if Clippie::Cucumber.is_overly_helpful?
+  end
 
-at_exit do
-  step_definitions = Clippie::Cucumber::UndefinedSteps.steps.map {|step|
-    multiline_arg_class = step.multiline_arg.nil? ? nil : step.multiline_arg.class
-      Cucumber::Cli::Main.step_mother.snippet_text(step.keyword, step.name, multiline_arg_class)
-    }
-  Clippie.says "I noticed you have some undefined steps"
-  Clippie.says "Would you like me to create them for you [Yn]?"
-  input = gets
-  if(input =~ /^n/)
-    Clippie.says "Awwww...Okay. :("
-  else
-    Clippie.says "Yay! I'm creating them for you now."
-    Clippie::Cucumber.define_steps(step_definitions.join("\n\n"))
+  at_exit do
+    create_steps(Clippie::Cucumber::UndefinedSteps.steps) unless Clippie::Cucumber.is_overly_helpful?
   end
 end
