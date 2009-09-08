1359e356acb89d8901377a1a348a60da66914558
sarah <sarahg.gray@gmail.com>
2009-09-07 23:59:23 -0500

    only spit out the name and keyword

--- lib/clippie/cucumber.rb
+++ lib/clippie/cucumber.rb
@@ -52,14 +52,14 @@ if respond_to?(:After)
       multiline_arg_class = step.multiline_arg.nil? ? nil : step.multiline_arg.class
         Cucumber::Cli::Main.step_mother.snippet_text(step.keyword, step.name, multiline_arg_class)
       }
-    Clippie.says "I noticed you have some undefined steps"
+    Clippie.says "I noticed you have some undefined steps."
     if show_undefined_steps
-      puts step_definitions.join("\n\n")
+      puts steps.map {|step| "\t\"#{step.keyword} #{step.name}\""}.join("\n")
     end
     Clippie.says "Would you like me to create them for you [Yn]?"
     input = gets
     if(input =~ /^n/)
-      Clippie.says "Awwww...Okay. I'll stop now.  I'll just leave you alone."
+      Clippie.says "Awwww...Okay. I'll stop now.  I'll just leave you alone. Paste them yourself."
     else
       Clippie.says "Yay! I'm creating them for you now."
       Clippie::Cucumber.define_steps(step_definitions.join("\n\n"))
