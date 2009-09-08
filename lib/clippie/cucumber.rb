d61cd0fb9ef0e50d34e333dbb6b325718993f34c
sarah <sarahg.gray@gmail.com>
2009-09-07 23:18:49 -0500

    clippie now says stuff

--- lib/clippie/cucumber.rb
+++ lib/clippie/cucumber.rb
@@ -22,15 +22,31 @@ module Clippie
     def self.step_definitions(path)
       self.step_definitions_root = path
     end
+    class UndefinedSteps
+      def self.add(step)
+        (@undefined_steps ||= []) << step
+      end
+      def self.steps
+        @undefined_steps
+      end
+    end
   end
+  
 end
 
 if respond_to?(:After)
   After do |scenario|
-    step_definitions = Cucumber::Cli::Main.step_mother.steps(:undefined).map {|step|
-      multiline_arg_class = step.multiline_arg.nil? ? nil : step.multiline_arg.class
-        Cucumber::Cli::Main.step_mother.snippet_text(step.keyword, step.name, multiline_arg_class)
-      }
-    Clippie::Cucumber.define_steps(step_definitions.join("\n\n"))
+    Cucumber::Cli::Main.step_mother.steps(:undefined).each {|step| Clippie::Cucumber::UndefinedSteps.add(step)}
   end
+end
+
+at_exit do
+  step_definitions = Clippie::Cucumber::UndefinedSteps.steps.map {|step|
+    multiline_arg_class = step.multiline_arg.nil? ? nil : step.multiline_arg.class
+      Cucumber::Cli::Main.step_mother.snippet_text(step.keyword, step.name, multiline_arg_class)
+    }
+  puts "CLIPPIE!\n"
+  puts "Adding the following steps to clippie_steps.rb\n"
+  puts step_definitions.join("\n\n")
+  Clippie::Cucumber.define_steps(step_definitions.join("\n\n"))
 end
